bdb72a7fc1cef5c7e02f67ec0e074c72adc46f59
youssefTarek <youssef3tarek@gmail.com>
2023-09-10 15:00:14 -0700

    update ButtonBlog

--- src/pages/singlePostPages/ButtonBlogSection.jsx
+++ src/pages/singlePostPages/ButtonBlogSection.jsx
@@ -2,7 +2,7 @@ import { Box, Button, Link, useTheme } from "@mui/material";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import { useNavigate, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import axios from "axios";
 import { SxButton } from "./Custom/SxButton";
 import { CustomLink } from "./Custom/CustomLink";
@@ -14,13 +14,12 @@ const ButtonBlogSection = () => {
   const LinkClasses = CustomLink();
 
   const { id } = useParams();
-  const [post, setPost] = useState(null);
 
   useEffect(() => {
     axios
       .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((response) => {
-        setPost(response.data);
+        response.data;
       });
   }, [id]);
 
@@ -36,90 +35,76 @@ const ButtonBlogSection = () => {
   };
 
   return (
-    <>
-      <Box
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: { xs: "column", sm: "row" },
+        justifyContent: "space-between",
+        px: 5,
+      }}
+    >
+      <Button
+        id="prev"
+        className={classes.root}
         sx={{
-          display: "flex",
-          flexDirection: { xs: "column", sm: "row" },
-          justifyContent: "space-between",
-          px: 5,
+          width: { xs: "10rem", sm: "20rem" },
         }}
+        startIcon={
+          <KeyboardArrowLeftIcon
+            sx={{
+              position: "absolute",
+              left: { xs: 20, sm: 90 },
+              top: 75,
+            }}
+          />
+        }
+        variant="text"
+        color="inherit"
+        onClick={handlePrevious}
       >
-        {post && (
-          <>
-            <Button
-              id="prev"
-              className={classes.root}
-              sx={{
-                width: { xs: "10rem", sm: "20rem" },
-              }}
-              startIcon={
-                <KeyboardArrowLeftIcon
-                  sx={{
-                    position: "absolute",
-                    left: { xs: 20, sm: 90 },
-                    top: 75,
-                  }}
-                />
-              }
-              variant="text"
-              color="inherit"
-              onClick={handlePrevious}
-            >
-              <Link
-                underline="none"
-                color="inherit"
-                className={LinkClasses.root}
-              >
-                previous
-              </Link>
-              <Link
-                sx={{ color: theme.palette.ColorChange.main, fontSize: 17 }}
-                underline="hover"
-              >
-                Blog
-              </Link>
-            </Button>
-            <Button
-              id="next"
-              endIcon={
-                <KeyboardArrowRightIcon
-                  sx={{
-                    position: "absolute",
-                    right: { xs: 20, sm: 15 },
-                    top: 75,
-                  }}
-                />
-              }
-              variant="text"
-              color="inherit"
-              className={classes.root}
-              sx={{
-                width: { xs: "10rem", sm: "20rem" },
-              }}
-              onClick={handleNext}
-            >
-              <Link
-                underline="none"
-                color="inherit"
-                className={LinkClasses.root}
-              >
-                next
-              </Link>
-              <Link
-                sx={{
-                  color: theme.palette.ColorChange.main,
-                  fontSize: 17,
-                }}
-                underline="hover"
-              >
-                React Labs:What We`ve Been Working On -March 2023
-              </Link>
-            </Button>
-          </>
-        )}
-      </Box>
-    </>
+        <Link underline="none" color="inherit" className={LinkClasses.root}>
+          previous
+        </Link>
+        <Link
+          sx={{ color: theme.palette.ColorChange.main, fontSize: 17 }}
+          underline="hover"
+        >
+          Blog
+        </Link>
+      </Button>
+      <Button
+        id="next"
+        endIcon={
+          <KeyboardArrowRightIcon
+            sx={{
+              position: "absolute",
+              right: { xs: 20, sm: 15 },
+              top: 75,
+            }}
+          />
+        }
+        variant="text"
+        color="inherit"
+        className={classes.root}
+        sx={{
+          width: { xs: "10rem", sm: "20rem" },
+        }}
+        onClick={handleNext}
+      >
+        <Link underline="none" color="inherit" className={LinkClasses.root}>
+          next
+        </Link>
+        <Link
+          sx={{
+            color: theme.palette.ColorChange.main,
+            fontSize: 17,
+          }}
+          underline="hover"
+        >
+          React Labs:What We`ve Been Working On -March 2023
+        </Link>
+      </Button>
+    </Box>
   );
 };
 
